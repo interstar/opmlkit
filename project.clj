b7e0478ce3c5ba6c2508ce618daf6e813aafbb01
phil jones <interstar@gmail.com>
2015-03-03 12:05:08 -0300

    housekeeping

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject opmlkit "0.1.0-SNAPSHOT"
-  :description "OPML diff functions"
+(defproject opmlkit "0.1.0"
+  :description "toolkit of OPML functions"
   :url "http://project.thoughtstorms.info/opmlkit"
-  :license {:name "Gnu General Public License"
-            :url ""}
+  :license {:name "GNU LESSER GENERAL PUBLIC LICENSE"
+            :url "https://www.gnu.org/licenses/lgpl.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
 
   :aot [opmlkit.core]
